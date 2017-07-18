e9a995fc05b031fcd703cb560df9e96be746ccb7
Karim BOUBRIT <karim.boubrit@neuros.fr>
2017-07-18 16:02:58 +0200

    Adding Rest bundle, correcting Abstract Controller issue from 3.3.4, updating to 3.3.5

--- GustoCoffe/src/AppBundle/Controller/DefaultController.php
+++ GustoCoffe/src/AppBundle/Controller/DefaultController.php
@@ -6,7 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends AbstractController
+class Controller extends AbstractController
 {
     /**
      * @Route("/", name="homepage")
